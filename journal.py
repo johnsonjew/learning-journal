ab9e80b49659acbfecc8f82e39ed7be119ce95d7
johnsonjjew@gmail.com <johnsonjjew@gmail.com>
2015-06-24 19:58:33 -0700

    step 2

--- journal.py
+++ journal.py
@@ -1,42 +1,67 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from sqlalchemy.orm import scoped_session, sessionmaker
+from zope.sqlalchemy import ZopeTransactionExtension
 import os
 from pyramid.config import Configurator
 from pyramid.view import view_config
 from waitress import serve
 import sqlalchemy as sa
 from sqlalchemy.ext.declarative import declarative_base
+import datetime
 from sqlalchemy import create_engine
 
+DBSession = scoped_session(sessionmaker(extension=ZopeTransactionExtension()))
 Base = declarative_base()
 
-# add this import at the top:
-import datetime
 
-# and define your class below the Base
 class Entry(Base):
     __tablename__ = 'entries'
+
     id = sa.Column(sa.Integer, primary_key=True, autoincrement=True)
-    title = sa.Column(sa.Unicode(127), nullable=False)
+    title = sa.Column(sa.Unicode(128), nullable=False)
     text = sa.Column(sa.UnicodeText, nullable=False)
-    date = sa.Column(
-        sa.DateTime, nullable=False, default=datetime.datetime.utcnow
-    )
+    date = sa.Column(sa.DateTime, nullable=False,
+                        default=datetime.datetime.utcnow)
+
+    @classmethod
+    def write(cls, title=None, text=None, session=None):
+        if session is None:
+            session = DBSession
+        instance = cls(title=title, text=text)
+        session.add(instance)
+        return instance
+
+    @classmethod
+    def all(cls, session=None):
+        if session is None:
+            session = DBSession
+        return session.query(cls).order_by(cls.date.desc()).all()
+
+    def __repr__(self):
+        return "<Entry(title='%s', date='%s')>" % (
+            self.title, self.date)
 
 DATABASE_URL = os.environ.get(
     'DATABASE_URL',
     'postgresql://johnson:password@localhost:5432/learning-journal'
 )
 
+
 def init_db():
-    engine = sa.create_engine(DATABASE_URL)
+    engine = create_engine(DATABASE_URL)
     Base.metadata.create_all(engine)
 
 
-@view_config(route_name='home', renderer='string')
-def home(request):
-    return "Hello World"
+@view_config(route_name='home', renderer='templates/lists.jinja2')
+def list_view(request):
+    entries = Entry.all()
+    return {'entries': entries}
 
+
+# @view_config(route_name='home', renderer='string')
+# def home(request):
+#     return "Hello World"
 
 def main():
     """Create a configured wsgi app"""
@@ -44,17 +69,22 @@ def main():
     debug = os.environ.get('DEBUG', True)
     settings['reload_all'] = debug
     settings['debug_all'] = debug
+    if not os.environ.get('TESTING', False):
+        engine = sa.create_engine(DATABASE_URL)
+        DBSession.configure(bind=engine)
     # configuration setup
     config = Configurator(
         settings=settings
-    )
+        )
+    config.include('pyramid_tm')
+    config.include('pyramid_jinja2')
     config.add_route('home', '/')
+    config.add_route('other', '/other/{special_val}')
     config.scan()
     app = config.make_wsgi_app()
     return app
 
-
 if __name__ == '__main__':
     app = main()
-    port = os.environ.get('PORT', 5000)
+    port = os.environ.get('PORT', 8080)
     serve(app, host='0.0.0.0', port=port)
